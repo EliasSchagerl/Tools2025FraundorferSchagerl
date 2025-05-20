01a231902818a15b841cf4b21622a9f4fab38c0c
jakef <s2410237011@fhooe.at>
2025-05-20 21:31:52 +0200

    added some more code

--- Fraundorfer_Schagerl_Android/dummy.java
+++ Fraundorfer_Schagerl_Android/dummy.java
@@ -8,7 +8,11 @@ public class dummy {
         int x = 4;
     }
 
-    public int add(int x, int y) {
+    public int add(int x, int y) { // DON'T TOUCH! I don't know what this method does, but it makes everything work...
         return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("7 + 3 = " + add(7+3)); // = 10? I think...
     }
 }
